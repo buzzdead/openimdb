fcff356f75f0205078b53b65d5f4644b56252f07
Sigmund.Volden <sigmund.volden@student.uib.no>
2022-01-05 10:37:26 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Component } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import SearchTerm from "./components/SearchTerm";
+import { IMovieList, IMovie } from "./Types";
 
 function App() {
+  const [movies, setMovies] = useState<IMovieList["movies"]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const url = `http://www.omdbapi.com/?apikey=6d684c9&s=${searchTerm}`;
+
+  const getMovie = async () => {
+    try {
+      const response = await fetch(url).then((response) => response.json());
+      const movieResponse = response.Search as IMovieList["movies"];
+
+      const promises = movieResponse.map(async (movie) => {
+        const response = await fetch(
+          `http://www.omdbapi.com/?i=${movie.imdbID}&apikey=6d684c9`
+        ).then((response) => response.json());
+        const movieRes = response as IMovie;
+        if (movieRes.Metascore.toString() == "N/A") movieRes.Metascore = 0;
+        return movieRes;
+      });
+      const updatedMovies = (await Promise.all(
+        promises
+      )) as IMovieList["movies"];
+      setMovies([...updatedMovies]);
+    } catch (e) {
+      console.error(e as Error);
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, [searchTerm]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList movies={movies} setMovies={setMovies} />
+      <SearchTerm searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
     </div>
   );
 }
