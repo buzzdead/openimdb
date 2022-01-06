dcdf976190ff2e69fa7fb6175031834e043f2bbe
Sigmund.Volden <sigmund.volden@student.uib.no>
2022-01-06 08:36:14 +0100

    Filters (refactor needed)

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { IMovieList, IMovieModal } from "../Types";
 
 let sorted = { Title: false, Year: false, Type: false, imdbRating: false };
+const filters = ["movie", "game", "series"]
 
 interface Props {
   movies: IMovieList["movies"];
@@ -10,13 +11,16 @@ interface Props {
 
 const MovieList: React.FC<Props> = ({ movies, setMovies }) => {
 
-const handleClick = (e: any) => {
+const handleMouseOver = (e: any) => {
   setMovieModal(e as IMovieModal)
 }
 
 const [movieModal, setMovieModal] = useState<IMovieModal>();
+const [filter, setFilter] = useState(
+  new Array(filters.length).fill(true)
+);
 
-  const handleChange = (e: any) => {
+  const onClick = (e: any) => {
     let sortedMovies;
     let type: "Title" | "Year" | "Type" | "imdbRating" = e.target.id;
     if (sorted[type]) {
@@ -44,8 +48,23 @@ const [movieModal, setMovieModal] = useState<IMovieModal>();
     setMovies([...sortedMovies]);
   };
 
+  const handleOnChange = (pos: number) => {
+    const updatedCheckedState = filter.map((item, index) =>
+      index === pos ? !item : item
+    );
+    setFilter(updatedCheckedState);
+   }
+
   const renderMovieList = (): JSX.Element[] => {
-    return movies.map((movie, key) => {
+    let newFilters: string[] = []
+    filters.forEach((ele, id) => {
+      if (filter[id]) newFilters.push(ele)
+    })
+    const filtered = movies.filter((ele) => {
+      if(newFilters.includes(ele.Type))
+        return ele
+    })
+    return filtered.map((movie, key) => {
       return (
         <tr key={key}>
           <img
@@ -57,36 +76,45 @@ const [movieModal, setMovieModal] = useState<IMovieModal>();
             }
             alt=""
           ></img>
-          <td onMouseLeave={() => handleClick(undefined)} onMouseEnter={() => handleClick(movie)}>{movie.Title}</td>
+          <td onMouseLeave={() => handleMouseOver(undefined)} onMouseEnter={() => handleMouseOver(movie)}>{movie.Title}</td>
           <td>{movie.Year}</td>
           <td>{movie.Type}</td>
-          <td>{movie.imdbRating}/10 &nbsp;&nbsp;<span>from {movie.imdbVotes} users</span></td>
+          <td><span id="rating">{movie.imdbRating}/10</span> &nbsp;&nbsp;<span>from {movie.imdbVotes} users</span></td>
         </tr>
       );
     });
   };
+
   return (
     <div>
-      {!movieModal && <h1 className="modal">Modal</h1>}
-      {movieModal && <h1 className="modal">{movieModal.Title}</h1>}
+      <div className="checkbox-container">
+         {filters.map((element, index) => {
+           return (
+            <div>
+              <input value={element} name={element} type="checkbox" checked={filter[index]} onChange={() => handleOnChange(index)} />
+              <label htmlFor={`custom-checkbox-${index}`}>{element}</label>
+           </div>
+         )})}
+       </div>
       <table>
         <tr>
           <th></th>
-          <th id="Title" onClick={handleChange}>
+          <th id="Title" onClick={onClick}>
             Title &#x21f5;
           </th>
-          <th id="Year" onClick={handleChange}>
+          <th id="Year" onClick={onClick}>
             Year &#x21f5;
           </th>
-          <th id="Type" onClick={handleChange}>
+          <th id="Type" onClick={onClick}>
             Type &#x21f5;
           </th>
-          <th id="imdbRating" onClick={handleChange}>
-            imdbRating &#x21f5;
+          <th id="imdbRating" onClick={onClick}>
+            Rating &#x21f5;
           </th>
         </tr>
         {renderMovieList()}
       </table>
+      {movieModal && <h1 className="modal">{movieModal.Title}</h1>}
     </div>
   );
 };
