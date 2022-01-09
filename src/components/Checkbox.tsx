0e8ed1bfb1ed7e08e13c685c52bf9c18e950c72e
Sigmund.Volden <sigmund.volden@student.uib.no>
2022-01-09 02:19:49 +0100

    Refactors

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import {IMovieList} from '../Types'
+
+const filters = ["movie", "game", "series"];
+
+interface Props {
+    movies: IMovieList["movies"];
+    setFilteredMovies: React.Dispatch<React.SetStateAction<IMovieList["movies"]>>;
+    favoriteMovies: IMovieList["movies"];
+  }
+
+const Checkbox: React.FC<Props> = ({movies, setFilteredMovies, favoriteMovies}) => {
+    
+    const [filter, setFilter] = useState(new Array(filters.length).fill(true));
+    const [favoriteToggle, setFavoriteToggle] = useState(false)
+
+    const handleOnClick = () => {
+        if(!favoriteToggle)
+            setFilteredMovies(favoriteMovies)
+        else
+            setFilteredMovies(movies)
+        setFavoriteToggle(!favoriteToggle)
+    }
+
+    useEffect(() => {
+        let newFilters: string[] = [];
+        filters.forEach((ele, id) => {
+          if (filter[id]) newFilters.push(ele);
+        });
+        let filtered;
+        if(favoriteToggle) {
+            filtered = favoriteMovies.filter((ele) => {
+                if (newFilters.includes(ele.Type)) return ele;
+              });
+        }
+        else {
+        filtered = movies.filter((ele) => {
+          if (newFilters.includes(ele.Type)) return ele;
+        })};
+        setFilteredMovies(filtered)
+    }, [movies, filter, favoriteToggle])
+
+    const handleOnChange = (pos: number) => {
+        const updatedCheckedState = filter.map((item, index) =>
+          index === pos ? !item : item
+        );
+        setFilter(updatedCheckedState);
+      };
+
+
+    return (
+        <div>
+          <div className="checkbox-container">
+            {filters.map((element, index) => {
+              return (
+                <div>
+                  <input
+                    value={element}
+                    name={element}
+                    type="checkbox"
+                    checked={filter[index]}
+                    onChange={() => handleOnChange(index)} />
+                  <label htmlFor={`custom-checkbox-${index}`}>{element}</label>
+                </div>
+              );
+            })}
+          </div>
+          <button onClick={handleOnClick}>Switch</button>
+        </div>
+    )}
+
+export default Checkbox;
---
+++ src/components/Table/MovieList.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { IMovieList, IMovieModal, IMovie } from "../../Types";
+import Checkbox from "../Checkbox";
+import Modal from "../Modal";
+import Table from "./Table";
+
+const filters = ["movie", "game", "series"];
+
+interface Props {
+  movies: IMovieList["movies"];
+  setMovies: React.Dispatch<React.SetStateAction<IMovieList["movies"]>>;
+}
+
+const MovieList: React.FC<Props> = ({ movies, setMovies }) => {
+  const [movieModal, setMovieModal] = useState<IMovieModal>();
+  const [filteredMovies, setFilteredMovies] = useState<IMovieList["movies"]>([]);
+  const [favoriteMovies, setFavoriteMovies] = useState<IMovieList["movies"]>([]);
+  const [modalPos, setModalPos] = useState<{x: number, y:number}>({x: 0, y: 0,});;
+
+  const handleMouseOver = (e: IMovieModal | undefined) => {
+    setMovieModal(e);
+  };
+
+  const handleOnClick = (movie: IMovie) => {
+    setFavoriteMovies([...favoriteMovies, movie])
+  }
+
+  const renderMovieList = (): JSX.Element[] => {
+     
+    return filteredMovies.map((movie, key) => {
+      return (
+        <tr key={key}>
+          <a
+            href={`https://www.imdb.com/title/${movie.imdbID}/?ref_=fn_al_tt_1`}
+            target="_blank"
+          >
+            <img
+              className="poster"
+              src={
+                movie.Poster
+                  ? movie.Poster
+                  : "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-696x1024.jpg"
+              }
+              alt=""
+            ></img>
+          </a>
+          <td
+            onClick={() => handleOnClick(movie)}
+            onMouseLeave={() => handleMouseOver(undefined)}
+            onMouseEnter={() =>
+              handleMouseOver(movie as unknown as IMovieModal)
+            }
+          >
+            {movie.Title}
+          </td>
+          <td>{movie.Year}</td>
+          <td>{movie.Type}</td>
+          <td>
+            <span id="rating">{movie.imdbRating}/10</span> &nbsp;&nbsp;
+            <span>from {movie.imdbVotes} users</span>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <Checkbox movies={movies} setFilteredMovies={setFilteredMovies} favoriteMovies={favoriteMovies}/>
+      <table>
+      <Table filteredMovies={filteredMovies} setFilteredMovies={setFilteredMovies} />
+      {renderMovieList()}
+      </table>
+      <Modal movieModal={movieModal} modalPos={modalPos} setModalPos={setModalPos} /> 
+      </div>)}
+
+export default MovieList;
